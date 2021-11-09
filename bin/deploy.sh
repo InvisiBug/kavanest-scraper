d44cd0a585d7034fb778aa324cc030255f847e98
InvisiBug <matthewjkavanagh@gmail.com>
2021-11-09 00:53:50 +0000

    Make deploy create an install if one doesnt already exist

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# yarn build && docker-compose up --build
-# clear && cd helm && try { helm upgrade kavanest-scraper . --namespace kavanest --create-namespace } catch || { helm install kavanest-scraper . --namespace kavanest --create-namespace }
 clear && cd helm && helm upgrade kavanest-scraper . --namespace kavanest --create-namespace
 
 EXITCODE=$?
