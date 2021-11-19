08b15d2112a9bebf9a9137fd3787a7075393115c
InvisiBug <matthewjkavanagh@gmail.com>
2021-11-19 11:56:49 +0000

    Finished switch to mondodb driver

--- src/components/database/index.ts
+++ src/components/database/index.ts
@@ -1,12 +1,12 @@
 import Mongo from "./mongo";
 
-const db = "mongo2";
+const db = "devices";
 
+export const rgbLightStore = new Mongo(db, "rgbLight").collection;
 export const radiatorStore = new Mongo(db, "radiator").collection;
 export const offsetStore = new Mongo(db, "offsets").collection;
 export const sensorStore = new Mongo(db, "sensors").collection;
 export const valveStore = new Mongo(db, "valves").collection;
 export const plugStore = new Mongo(db, "plugs").collection;
-export const rgbLightStore = new Mongo(db, "rgbLight").collection;
 
 export const options = { new: true, upsert: true };
--- src/components/devices/components/offsets.ts
+++ src/components/devices/components/offsets.ts
@@ -1,5 +1,5 @@
 import { MqttClient } from "mqtt";
-import { offsetStore, options } from "../../database";
+import { offsetStore, options, sensorStore } from "../../database";
 
 export default class Offsets {
   client: MqttClient;
@@ -13,22 +13,10 @@ export default class Offsets {
   async handleIncoming(topic: String, rawPayload: Object) {
     if (topic === "roomOffsets") {
       const payload = JSON.parse(rawPayload.toString());
-      console.log(payload);
 
-      await offsetStore.findOneAndUpdate(
-        { name: "roomOffsets" },
-        {
-          $set: {
-            name: "roomOffsets",
-            livingRoom: payload.livingRoom,
-            kitchen: payload.kitchen,
-            liamsRoom: payload.liamsRoom,
-            study: payload.study,
-            ourRoom: payload.ourRoom,
-          },
-        },
-        options,
-      );
+      for (const room in payload) {
+        await sensorStore.findOneAndUpdate({ room }, { $set: { offset: payload[room] } });
+      }
     }
   }
 }
