7b8b30866844d9bf19df67c57ac3c16e2837638c
InvisiBug <matthewjkavanagh@gmail.com>
2021-12-01 17:24:51 +0000

    Using device config propperly

--- src/components/database/index.ts
+++ src/components/database/index.ts
@@ -2,8 +2,8 @@ import Mongo from "./mongo";
 
 const db = "devices";
 
-export const rgbLightStore = new Mongo(db, "rgbLight").collection;
-export const radiatorStore = new Mongo(db, "radiator").collection;
+export const rgbLightStore = new Mongo(db, "rgbLights").collection;
+export const radiatorStore = new Mongo(db, "radiators").collection;
 export const offsetStore = new Mongo(db, "offsets").collection;
 export const sensorStore = new Mongo(db, "sensors").collection;
 export const valveStore = new Mongo(db, "valves").collection;
