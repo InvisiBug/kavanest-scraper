6423e73c011d4cb1f3567901ee344375bf91675d
InvisiBug <matthewjkavanagh@gmail.com>
2021-12-24 20:55:19 +0000

    Added socket to rgbLight

--- src/components/devices/components/rgbLight.ts
+++ src/components/devices/components/rgbLight.ts
@@ -1,13 +1,15 @@
 import { MqttClient } from "mqtt";
 import { rgbLightStore, options } from "../../database";
 import { disconnectWatchdog } from "../../helpers";
-import { rgbLightData } from "../../../types";
+import { Socket } from "socket.io";
 
 export default class RGBLight {
   client: MqttClient;
   timer: NodeJS.Timeout;
   topic: string;
   name: string;
+  io: Socket;
+  mongoID: string = "";
 
   data: rgbLightData = {
     name: null,
@@ -16,10 +18,12 @@ export default class RGBLight {
     blue: null,
     mode: null,
     connected: false,
+    _id: null,
   };
 
-  constructor(client: MqttClient, deviceConfig: any) {
+  constructor(client: MqttClient, deviceConfig: any, io: Socket) {
     this.client = client;
+    this.io = io;
 
     this.name = deviceConfig.name;
     this.topic = deviceConfig.topic;
@@ -39,9 +43,15 @@ export default class RGBLight {
           blue: payload.blue,
           mode: payload.mode,
           connected: true,
+          _id: this.mongoID,
         };
 
-        writeToMongo(this.data);
+        writeToMongo(this.data).then((id) => {
+          this.mongoID = id;
+        });
+
+        this.io.emit(this.mongoID, this.data);
+
         clearTimeout(this.timer);
         this.timer = disconnectWatchdog(this.data, `${this.name} disconnected`, writeToMongo);
       } catch (error) {
@@ -52,13 +62,25 @@ export default class RGBLight {
 }
 
 const writeToMongo = async (data: rgbLightData) => {
-  await rgbLightStore.findOneAndUpdate(
-    { name: data.name },
-    {
-      $set: { ...data },
-    },
-    options,
-  );
+  let id: string = "";
+
+  await rgbLightStore
+    .findOneAndUpdate(
+      { name: data.name },
+      {
+        $set: { ...data },
+      },
+      options,
+    )
+    .then((mongoDoc) => {
+      if (mongoDoc.value) {
+        if (Object(mongoDoc).constructor !== Promise) {
+          id = mongoDoc.value._id.toString();
+        }
+      }
+    });
+
+  return id;
 };
 
 interface MQTTpalyoad {
@@ -67,4 +89,14 @@ interface MQTTpalyoad {
   green: number;
   blue: number;
   mode: number;
+}
+
+interface rgbLightData {
+  name: string | null;
+  red: number | null;
+  green: number | null;
+  blue: number | null;
+  mode: number | null;
+  connected: boolean;
+  _id: string | null;
 }
