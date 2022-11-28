33fd333b41dfaa4d4072a547cf285eb4a5c86041
InvisiBug <matthewjkavanagh@gmail.com>
2022-11-28 20:16:40 +0000

    wip

---
+++ bin/push--live.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+REGISTRY_IP=192.168.1.61:5000
+yarn install && \
+yarn build && \
+docker build -f ./Dockerfile.kube -t kavanest-scraper . && \
+docker tag kavanest-scraper-live $REGISTRY_IP/kavanest-scraper-live && \
+docker push $REGISTRY_IP/kavanest-scraper && \
+rm -r dist
--- bin/push--test.sh
+++ bin/push--test.sh
@@ -4,6 +4,6 @@ REGISTRY_IP=192.168.1.61:5000
 yarn install && \
 yarn build && \
 docker build -f ./Dockerfile.kube -t kavanest-scraper . && \
-docker tag kavanest-scraper $REGISTRY_IP/kavanest-scraper && \
+docker tag kavanest-scraper-test $REGISTRY_IP/kavanest-scraper-test && \
 docker push $REGISTRY_IP/kavanest-scraper && \
 rm -r dist
