1d5e3c2af16dab3b1a9cb2f880014ec9dfbc7875
InvisiBug <matthewjkavanagh@gmail.com>
2022-11-28 21:05:05 +0000

    Added image tags

--- bin/push--live.sh
+++ bin/push--live.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 REGISTRY_IP=192.168.1.61:5000
+APP_NAME=kavanest-scraper:live
+
 yarn install && \
 yarn build && \
-docker build -f ./Dockerfile.kube -t kavanest-scraper . && \
-docker tag kavanest-scraper-live $REGISTRY_IP/kavanest-scraper-live && \
-docker push $REGISTRY_IP/kavanest-scraper && \
+docker build -f ./Dockerfile.kube -t $APP_NAME . && \
+docker tag $APP_NAME $REGISTRY_IP/$APP_NAME && \
+docker push $REGISTRY_IP/$APP_NAME && \
 rm -r dist
--- bin/push--test.sh
+++ bin/push--test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 REGISTRY_IP=192.168.1.61:5000
+APP_NAME=kavanest-scraper:test
+
 yarn install && \
 yarn build && \
-docker build -f ./Dockerfile.kube -t kavanest-scraper . && \
-docker tag kavanest-scraper-test $REGISTRY_IP/kavanest-scraper-test && \
-docker push $REGISTRY_IP/kavanest-scraper && \
+docker build -f ./Dockerfile.kube -t $APP_NAME . && \
+docker tag $APP_NAME $REGISTRY_IP/$APP_NAME && \
+docker push $REGISTRY_IP/$APP_NAME && \
 rm -r dist
